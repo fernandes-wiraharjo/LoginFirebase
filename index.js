c2fcf207d9074415236b86bdbf8742f4219a03c9
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2019-01-10 21:29:55 +0700

    push login firebase 1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-/** @format */
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { config } from "./credentials/firebaseCredentials";
+import firebase from "@firebase/app";
+import "@firebase/auth";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import { config } from "./credentials/firebaseCredentials"
-
-if(!firebase.apps.length){
-    firebase.initializeApp(config);
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
 }
 
 AppRegistry.registerComponent(appName, () => App);
